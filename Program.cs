0174bf943657ffa5c9252f24edc7f9f0e3c6c431
PatriceHANNE <patrice.hanne@gmail.com>
2023-12-19 10:32:11 +0100

    ADD :  Branche Exemple pour test

--- Program.cs
+++ Program.cs
@@ -4,9 +4,13 @@ namespace CsharpAdvanced // Tiroir
 {
     internal class Program
     {
+        public static int age = 10;
+        public static string nom = "Toto";
+
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Exemple, {0}, bla bla {1}", nom, age);
         }
     }
 }
